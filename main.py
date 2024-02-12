14863f0ff24e5c6b4565a16540284a5979ad5d4e
sihioov <sihioov@gmail.com>
2024-02-12 15:56:07 +0900

    rt json

--- main.py
+++ main.py
@@ -46,14 +46,18 @@ async def post_resource(request: Request, url1: str, url2: str):
     global isSetAPI, clientApiURL
     if isSetAPI:
         targetApiURL = f"{clientApiURL}/{url1}/{url2}"
+        
         data = await request.json()  # 한 번만 호출
+        print(data)
         #headers = dict(request.headers)
         #headers.pop("host", None)  # 필요에 따라 헤더 수정
-
+        
+        #print(request.headers)
         try:
             async with httpx.AsyncClient() as client:
                 response = await client.post(targetApiURL, json=data)#, headers=headers)
                 print(response.json())  # 타겟 서버의 응답 반환
+                return response.json();
         except httpx.RequestError as exc:
             print(f"An error occurred while requesting {exc.request.url!r}.")
             return {"error": "Request failed"}
